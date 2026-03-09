e36aaaa2e6f2f2eb1fa30ab0083944063ed399d6
yoonseo <78381554+yoonseo4343@users.noreply.github.com>
2026-03-09 09:55:03 +0900

    [level 2] Title: 피보나치 수, Time: 0.50 ms, Memory: 4.59 MB -BaekjoonHub

---
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int arr[100001];
+    arr[0]=0;
+    arr[1]=1;
+    for(int i=2;i<=n;i++){
+        arr[i]=(arr[i-1]+arr[i-2])%1234567;
+    }
+    return arr[n];
+}
