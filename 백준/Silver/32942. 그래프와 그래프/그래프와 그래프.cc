04da0ab96a6af63a31d5cea37c04903b17bbae49
yoonseo <78381554+yoonseo4343@users.noreply.github.com>
2026-03-11 10:31:54 +0900

    [Silver V] Title: 그래프와 그래프, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/32942. 그래프와 그래프/그래프와 그래프.cc
+++ 백준/Silver/32942. 그래프와 그래프/그래프와 그래프.cc
@@ -4,25 +4,12 @@ using namespace std;
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    float a;
-    float b;
-    float c;
+    int a,b,c;
     cin>>a>>b>>c;
     for(int i=1;i<11;i++){
-        if(b==0){
-            if(i==c/a){
-                cout<<"1 2 3 4 5 6 7 8 9 10"<<"\n";
-                continue;
-            }
-            else{
-                cout<<0<<"\n";
-                continue;
-            }
-        }
-        float tmp=(c-a*i)/b;
         bool isFind=false;
         for(int j=1;j<11;j++){
-            if(j==tmp){
+            if(a*i+b*j==c){
                 if(isFind)
                     cout<<" ";
                 cout<<j;
