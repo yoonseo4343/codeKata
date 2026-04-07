efe777d7a2c7d63e9b2b038f67399e14bbedf775
yoonseo <78381554+yoonseo4343@users.noreply.github.com>
2026-04-07 12:47:52 +0900

    [level 2] Title: 배달, Time: 0.29 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -14,32 +14,31 @@ int solution(int N, vector<vector<int> > road, int K) {
         info[it[1]].push_back({it[0],it[2]});
     }
     
-    vector<int> visited(N+1, 1e9);// 가능한 곳 체크
+    vector<int> visited(N+1, 0);// 가능한 곳 체크
     
-    // queue<tuple<int,int>> que;// 위치 및 가중치
-    priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
-    pq.push({0,1});// 거리, 노드
+    queue<tuple<int,int>> que;// 위치 및 가중치
+    que.push({1,0});
     visited[1]=1;
-    while(!pq.empty()){
-        auto[w,c]=pq.top();
-        pq.pop();
+    while(!que.empty()){
+        auto[c,w]=que.front();
+        que.pop();
         for(auto it:info[c]){
             auto[nx,t]=it;// 다음 노드, 시간
+            // 1은 안됨
+            if(nx==1)
+                continue;
             // 방문체크
-            if(visited[nx]<w+t)
+            if(visited[nx]!=0&&visited[nx]<w+t)
                 continue;
             // 가중치 체크
             if(w+t>K)
                 continue;
-            pq.push({w+t,nx});
+            que.push({nx,w+t});
             visited[nx]=w+t;
         }
     }
 
-    for(auto it:visited){
-        if(it<=K)
-            answer++;
-    }
+    answer=N-count(visited.begin(), visited.end(), 0)+1;
     
     return answer;
 }
